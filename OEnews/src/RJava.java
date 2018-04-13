202e10238fd3c3a27558340600ae2f2de9303a00
LimFull <full7002@naver.com>
2018-04-13 18:34:23 +0900

    뉴스 내용 출력 안됨

--- OEnews/src/RJava.java
+++ OEnews/src/RJava.java
@@ -22,9 +22,20 @@ public static void main (String[] args){
 	re.eval("if(!requireNamespace(\"dplyr\")){install.packages(\"dplyr\")}");
 	re.eval("library(dplyr)");
 	re.eval("naver_url <- 'http://news.naver.com/main/list.nhn?mode=LSD&mid=sec&sid1=105'");
-	System.out.print(re.eval("naver_url"));
-	re.eval("");
-	re.eval("");
+	System.out.println(re.eval("naver_url")); //url 잘 들어갔는지 확인
+	re.eval("html <- read_html(naver_url)");
+	re.eval("temp <- c(unique(html_nodes(html,\"#main_content .list_body .type06_headline a\")%>%html_attr('href')),unique(html_nodes(html,\"#main_content .list_body .type06 a\")%>%html_attr('href')))");
+
+	re.eval("news_url <-c()");
+	re.eval("news_url <- c(news_url,temp)");
+	System.out.println(re.eval("news_url")); // news_url 확인
+	System.out.println("news_url 완료");
+	
+	re.eval("news_content <- c()");
+	re.eval("for (i in 1:length(news_url)){html <- read_html(news_url[i]) temp <- repair_encoding(html_text(html_nodes(html,'#articleBodyContents')),from = 'utf-8') news_content <- c(news_content,temp)}");
+	re.eval("news <- cbind(url=news_url,content=unlist(news_content))");
+	re.eval("news <- as.data.frame(news)");
+	System.out.println(re.eval("news"));
 	
 	System.out.println("\nend");
 }
