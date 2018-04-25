dc46e3dde0fb4fc020d91825fa5aaae8f1711556
LimFull <full7002@naver.com>
2018-04-25 14:52:12 +0900

    메인 패키지

---
+++ OEnews/src/main/Rmain.java
@@ -0,0 +1,11 @@
+package main;
+
+import itpart.Rit;
+import lifepart.Rlife;
+import societypart.Rsociety;
+
+public class Rmain {
+	public static void main(String[] args){
+		Rit rit = new Rit();
+	}
+}
