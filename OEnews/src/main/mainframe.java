b2c656ef39563788fe38bfa8bf5e59d266d41565
LimFull <full7002@naver.com>
2018-04-26 20:06:33 +0900

    경제,정치 버튼 이미지 추가

--- OEnews/src/main/mainframe.java
+++ OEnews/src/main/mainframe.java
@@ -18,20 +18,23 @@ public class mainframe extends JFrame {
 	a.setLayout(null);
 	JButton itbtn = new JButton(new ImageIcon("./img/normalit.png"));
 	JButton societybtn = new JButton(new ImageIcon("./img/normalsociety.png"));
+	JButton lifebtn = new JButton(new ImageIcon("./img/normallife.png"));
+	JButton politicsbtn = new JButton(new ImageIcon("./img/normalpolitics.png"));
+	JButton economybtn = new JButton(new ImageIcon("./img/normaleconomy.png"));
 	//itbtn.setPreferredSize(new Dimension(100,100)); //절대위치
 	//itbtn.setBounds(130,150,95,30);  //x,y,width,height
 	//itbtn.setText("");  //버튼 텍스트
 	itbtn.setPressedIcon(new ImageIcon("./img/pressedit.png"));societybtn.setPressedIcon(new ImageIcon("./img/pressedsociety.png"));  //버튼 클릭 이미지
-	itbtn.setSize(234,60);societybtn.setSize(234,60);   //버튼 크기
-	itbtn.setLocation(115,140);societybtn.setLocation(451,140);  //버튼위치
-	itbtn.setVisible(true);societybtn.setVisible(true);
-	itbtn.setBorderPainted(false);societybtn.setBorderPainted(false);  //버튼 외곽선
-	itbtn.setContentAreaFilled(false);societybtn.setContentAreaFilled(false);  //버튼 내용영역
-	itbtn.setFocusPainted(false);societybtn.setFocusPainted(false);  //버튼 선택 시 테두리
+	lifebtn.setPressedIcon(new ImageIcon("./img/pressedlife.png"));politicsbtn.setPressedIcon(new ImageIcon("./img/pressedpolitics.png"));
+	economybtn.setPressedIcon(new ImageIcon("./img/pressedeconomy.png"));
+	itbtn.setSize(234,60);societybtn.setSize(234,60);lifebtn.setSize(234,60);politicsbtn.setSize(234,60);economybtn.setSize(234,60);   //버튼 크기
+	itbtn.setLocation(115,140);societybtn.setLocation(451,140);lifebtn.setLocation(115,250);politicsbtn.setLocation(451,250);economybtn.setLocation(115,360);  //버튼위치
+	itbtn.setVisible(true);societybtn.setVisible(true);lifebtn.setVisible(true);politicsbtn.setVisible(true);economybtn.setVisible(true);
+	itbtn.setBorderPainted(false);societybtn.setBorderPainted(false);lifebtn.setBorderPainted(false);politicsbtn.setBorderPainted(false);economybtn.setBorderPainted(false);  //버튼 외곽선
+	itbtn.setContentAreaFilled(false);societybtn.setContentAreaFilled(false);lifebtn.setContentAreaFilled(false);politicsbtn.setContentAreaFilled(false);economybtn.setContentAreaFilled(false);  //버튼 내용영역
+	itbtn.setFocusPainted(false);societybtn.setFocusPainted(false);lifebtn.setFocusPainted(false);politicsbtn.setFocusPainted(false);economybtn.setFocusPainted(false);  //버튼 선택 시 테두리
 	
-	
-
-	a.add(itbtn); a.add(societybtn);
+	a.add(itbtn); a.add(societybtn); a.add(lifebtn); a.add(politicsbtn); a.add(economybtn);
 	this.add(a);
 	this.setSize(800,480); //라즈베리파이 7인치 터치 스크린 해상도 
 	this.setVisible(true);
