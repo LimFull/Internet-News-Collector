b8c3d0f90785b99632e88f9a473514c465deab24
임가득 <full7002@naver.com>
2018-04-27 17:25:07 +0900

    뒤로가기버튼

--- OEnews/src/itpart/itcontent.java
+++ OEnews/src/itpart/itcontent.java
@@ -12,7 +12,12 @@ public itcontent(){
 	GraphicsDevice gd = ge.getDefaultScreenDevice();
 	
 	panel a = new panel();
+	a.setLayout(null);
+	JButton backbtn = new JButton();
+	backbtn.setSize(80,30);
+	backbtn.setVisible(true);
 	
+	a.add(backbtn);
 	this.add(a);
 	this.setSize(800,480);
 	this.setVisible(true);
