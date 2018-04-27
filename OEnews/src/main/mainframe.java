978b6a1084442383feeabe6c0cf84dc2a78a5a5b
WJY <droid44@naver.com>
2018-04-27 17:26:06 +0900

    commit

--- OEnews/src/main/mainframe.java
+++ OEnews/src/main/mainframe.java
@@ -1,13 +1,16 @@
 package main;
 import javax.swing.*; //프로젝트 실행환경 - JavaSE-1.7 
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import itpart.*;
+import economypart.*;
 
 
 public class mainframe extends JFrame {
-	private static final String ACTION_COMMAND = "1";
+	private static final String ACTION_COMMAND_IT= "1";
+	private static final String ACTION_COMMAND_ECONOMY= "1";
  	
 	mainframe(){
 
@@ -38,20 +41,25 @@ public class mainframe extends JFrame {
 	
 	ActionListener listener = new ActionListener(){
 		public void actionPerformed(ActionEvent e){
-			if (e.getActionCommand().equals(ACTION_COMMAND)){
+			if (e.getActionCommand().equals(ACTION_COMMAND_IT)){
 				itcontent itc = new itcontent();
 				}
+			if (e.getActionCommand().equals(ACTION_COMMAND_ECONOMY)){
+				economycontent ecc = new economycontent();
+			}
 		}
 	};
 	
 	itbtn.addActionListener(listener);
+	economybtn.addActionListener(listener);
 	a.add(itbtn); a.add(societybtn); a.add(lifebtn); a.add(politicsbtn); a.add(economybtn);
 	this.add(a);
 	this.setSize(800,480); //라즈베리파이 7인치 터치 스크린 해상도 
 	this.setVisible(true);
 	gd.setFullScreenWindow(this);  //전체
 	
-	itbtn.setActionCommand(ACTION_COMMAND);
+	itbtn.setActionCommand(ACTION_COMMAND_IT);
+	economybtn.setActionCommand(ACTION_COMMAND_ECONOMY);
 
 	}//여기까지 생성자
  
