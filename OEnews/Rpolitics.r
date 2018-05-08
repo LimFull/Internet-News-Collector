0664742d58f23dcf4074c6f614bfd0680518ee4e
LeeHangseok <31423898+LeeHangseok@users.noreply.github.com>
2018-05-08 11:45:11 +0900

    마스터 충돌 변경
    마스터 충돌 변경

--- OEnews/Rpolitics.r
+++ OEnews/Rpolitics.r
@@ -49,24 +49,23 @@ library(stringr)
  html <- read_html(naver_url)
  temp <- c(unique(html_nodes(html,"#main_content .list_body .type06_headline a")%>%
                 html_attr('href')),unique(html_nodes(html,"#main_content .list_body .type06 a")%>%
-                                             html_attr('href')))
+                                             html_attr('href')),repair_encoding(html_text(html_nodes(html,'.writing')),from = 'utf-8'))
  temp
- 
+ speech<-temp[21:40] #언론사명 저장 
 
  news_url <-c()
- news_url <- c(news_url,temp)
+ news_url <- c(news_url,temp[1:20])
  news_url 
  
 
  news_content <- c()
-
 
  for (i in 1:length(news_url)){
      html <- read_html(news_url[i])
      temp <- repair_encoding(html_text(html_nodes(html,'#articleBodyContents')),from = 'utf-8')
      news_content <- c(news_content,temp)
  }
- news <- cbind(url=news_url,content=unlist(news_content))
+  news <- cbind(url=news_url,content=unlist(news_content))
  news <- as.data.frame(news)
  news
 
@@ -114,7 +113,7 @@ stopwords=c("오전","오후","","며","월","많","당","전","이날","대해"
 
  #많이 나온 단어의 링크 추출
  wordurl <- matrix(nrow=3, ncol=20)     #핫 키워드가 있는 기사의 url을 담을 벡터 생성
- wordtitle <- matrix(nrow=3, ncol=20) # 핫 키워드가 있는 기사의 title 벡터 생성
+ wordspeech <- matrix(nrow=3, ncol=20)     #핫 키워드가 있는 기사의 언론사명을 담을 행렬 생성
  sortedword <- doc[rev(order(rowSums(doc))),]           #총 등장 횟수(행의 합)를 기준으로 정렬한 sortedword 벡터 생성
  for (i in 1:3){                       # i : 몇 개의 키워드의 url을 뽑을 것인지 
  k <- 0
@@ -122,7 +121,7 @@ stopwords=c("오전","오후","","며","월","많","당","전","이날","대해"
 if (sortedword[i,j] != 0) {            # i번째 순위 키워드가 j번째 기사에 포함될 경우 (0이 아닌 경우)
 k <- k+1
 wordurl[i,k] <- c(news_url[j])        # wordurl[i]에 j번째 기사의 url을 넣는다.
-}}}        
+wordspeech[i,k] <- c(speech[j])}}}    # wordurl[i]에 j번째 기사의 언론사명을 넣는다.
 
  doc <- rowSums(doc) 
  doc <- doc[order(doc,decreasing=T)] 
@@ -143,5 +142,5 @@ wordurl
  dir.create("./Rdata",showWarnings = F) 
 #wordurl, keywords를 csv파일로 저장
  write.csv(wordurl,file=paste0("./Rdata/wordurl",".csv"),row.names = F)
+ write.csv(wordspeech,file=paste0("./Rdata/wordspeech",".csv"),row.names = F)
  write.table(keywords,file=paste0("./Rdata/keywords",".csv"),row.names = F,col.names = F)
- 
