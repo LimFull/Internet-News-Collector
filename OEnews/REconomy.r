05b3057d2211e0f0e190330833aa06563525398d
LimFull <full7002@naver.com>
2018-05-11 14:30:00 +0900

    오류 이전으로 되돌림

--- OEnews/REconomy.r
+++ OEnews/REconomy.r
@@ -1,7 +1,4 @@
-#!/usr/bin/Rscript --vanilla --slave 
-
-# rvest 패키지 필요 : 
-if (!requireNamespace("rvest")){
+f (!requireNamespace("rvest")){
   install.packages("rvest")
 } 
 library(rvest)
@@ -59,13 +56,11 @@ news_url
 
 
 news_content <- c()
-news_title <- c()
+
 for (i in 1:length(news_url)){
   html <- read_html(news_url[i])
   temp <- repair_encoding(html_text(html_nodes(html,'#articleBodyContents')),from = 'utf-8')
   news_content <- c(news_content,temp)
-  temp <- repair_encoding(html_text(html_nodes(html,'h3#articleTitle')),from = 'utf-8')
-  news_title <- c(news_title, temp)
 }
 news <- cbind(url=news_url,content=unlist(news_content))
 news <- as.data.frame(news)
@@ -112,9 +107,8 @@ doc <- TermDocumentMatrix(doc,control=list(tokenize=words,removeNumbers=T,remove
 doc <- as.matrix(doc)
 
  #많이 나온 단어의 링크 추출
- wordurl <- matrix(nrow=3, ncol=20)     #핫 키워드가 있는 기사의 url을 담을 벡터 생성
- wordspeech <- matrix(nrow=3, ncol=20)     #핫 키워드가 있는 기사의 언론사명을 담을 행렬 생성   
- wordtitle <- matrix(nrow=3, ncol=20)     #핫 키워드가 있는 기사의 제목을 담을 행렬 생성   
+ wordurl <- matrix(nrow=5, ncol=20)     #핫 키워드가 있는 기사의 url을 담을 벡터 생성
+ wordspeech <- matrix(nrow=5, ncol=20)     #핫 키워드가 있는 기사의 언론사명을 담을 행렬 생성   
  sortedword <- doc[rev(order(rowSums(doc))),]           #총 등장 횟수(행의 합)를 기준으로 정렬한 sortedword 벡터 생성
  for (i in 1:5){                       # i : 몇 개의 키워드의 url을 뽑을 것인지 
  k <- 0
@@ -122,17 +116,16 @@ doc <- as.matrix(doc)
 if (sortedword[i,j] != 0) {            # i번째 순위 키워드가 j번째 기사에 포함될 경우 (0이 아닌 경우)
 k <- k+1
 wordurl[i,k] <- c(news_url[j])        # wordurl[i]에 j번째 기사의 url을 넣는다.
-wordspeech[i,k] <- c(speech[j])       # wordurl[i]에 j번째 기사의 언론사명을 넣는다.
-wordtitle[i,k] <- c(news_title[j])}}}    # wordurl[i]에 j번째 기사의 제목을 넣는다.
+wordspeech[i,k] <- c(speech[j])}}}    # wordurl[i]에 j번째 기사의 언론사명을 넣는다.
 
 doc <- rowSums(doc) 
 doc <- doc[order(doc,decreasing=T)] 
 doc <- as.data.frame(doc[1:30])
 doc
 
-#워드클라우드 생성 
-pdf.options(family = "Korea1deb") #pdf 한글 옵션 
-png(filename="cloudit.png",width=500,height=500) #png 이미지 저장 
+#워드클라우드 생성 
+pdf.options(family = "Korea1deb") #pdf 한글 옵션 
+png(filename="cloudit.png",width=500,height=500) #png 이미지 저장 
 wordcloud(words = rownames(doc),freq = doc$doc, min.freq=1, max.words=200, random.order=FALSE,rot.per=0.3,colors=brewer.pal(5,"Dark2"), scale=c(7,3))
 
 #wordurl 확인
@@ -145,5 +138,4 @@ wordurl
 #wordurl, keywords를 csv파일로 저장
  write.csv(wordurl,file=paste0("./Rdata/ECONOMYwordurl",".csv"),row.names = F)
  write.csv(wordspeech,file=paste0("./Rdata/ECONOMYwordspeech",".csv"),row.names = F)
- write.csv(wordtitle,file=paste0("./Rdata/ECONOMYwordtitle",".csv"),row.names = F)
  write.table(keywords,file=paste0("./Rdata/ECONOMYkeywords",".csv"),row.names = F,col.names = F)
