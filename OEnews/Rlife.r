aad02172e242d703feffd0fbf8efea39538d1ba2
임가득 <full7002@naver.com>
2018-05-15 16:36:20 +0900

    특수문자 제거

--- OEnews/Rlife.r
+++ OEnews/Rlife.r
@@ -114,6 +114,7 @@ library(stringr)
  news_content<-gsub("연합뉴스\\)","",news_content)
  news_content<-gsub("[a-z]*@","",news_content)
  news_content<-gsub("【서울","",news_content)
+ news_content<-gsub("◇","",news_content)
 
  doc <- Corpus(VectorSource(news_content))
  doc <- TermDocumentMatrix(doc,control=list(tokenize=words,removeNumbers=T,removePunctuation=T,wordLengths=c(3,Inf),
