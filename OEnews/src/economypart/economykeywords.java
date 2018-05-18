d5038d250025634d9eef3692fe40b2a80cdc9b58
LimFull <full7002@naver.com>
2018-05-18 22:23:15 +0900

    각 카테고리 keywords클래스 추가(csv파일 읽기)

---
+++ OEnews/src/economypart/economykeywords.java
@@ -0,0 +1,45 @@
+package economypart;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class economykeywords {
+	List<String> ret = new ArrayList<String>(); //키워드를 저장할 문자열 리스트
+	
+	 public economykeywords(){
+	BufferedReader br = null;
+	try{
+		br = Files.newBufferedReader(Paths.get("Rdata/ITkeywords.csv")); //csv파일을 읽음  
+		Charset.forName("UTF-8");  //UTF-8 형식
+		String line = "";
+		
+		while((line = br.readLine()) != null){
+			ret.add(line);	// 한 줄씩 ret에 추가
+		}
+	
+	}catch(FileNotFoundException e){
+		e.printStackTrace();
+	}catch(IOException e){
+		e.printStackTrace();
+	}finally{
+		try{
+			if(br != null){
+				br.close();  //파일 읽기 종료
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	 }
+	 
+	 String getwords(int i){ //i번째 단어를 가져오는 메소드
+		  return ret.get(i);
+	 }
+}
