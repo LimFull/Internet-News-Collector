d6e558d2bf7c61a149223dfc3b5b6c3f486f8ea9
LimFull <full7002@naver.com>
2018-05-22 18:14:53 +0900

    언론사 넣기 (수정 중)

--- OEnews/src/main/Rmain.java
+++ OEnews/src/main/Rmain.java
@@ -9,12 +9,12 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 //import javax.swing.*; //프로젝트 실행환경 - JavaSE-1.7 
 //import java.awt.*;
- 
+ import itpart.itspeech;
 
 
 public class Rmain {
 	public static void main(String[] args){
-		mainframe mf = new mainframe();
-	
+		//mainframe mf = new mainframe();
+		itspeech its = new itspeech();
 	}
 }
