9efaeb120d4591cf279e564bec135f569fa63d83
LeeHangseok <31423898+LeeHangseok@users.noreply.github.com>
2018-05-27 14:09:22 +0900

    언론사 추가
    언론사 추가

--- OEnews/src/economypart/economycontent.java
+++ OEnews/src/economypart/economycontent.java
@@ -11,53 +11,140 @@ import java.awt.event.ActionListener;
 
 public class economycontent extends JFrame {
 	private static final String ACTION_COMMAND_BACK= "1";
-public economycontent(){
-	setUndecorated(true);
-	GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-	GraphicsDevice gd = ge.getDefaultScreenDevice();
-	
+	private static final String ACTION_COMMAND_ONE= "2";
+	private static final String ACTION_COMMAND_TWO= "3";
+	private static final String ACTION_COMMAND_THREE= "4";
+	private static final String ACTION_COMMAND_FOUR= "5";
+	private static final String ACTION_COMMAND_FIVE= "6";
+	economyspeech economys = new economyspeech(); //언론사 수 셈
 	panel a = new panel();
-	a.setLayout(null);
-	JButton backbtn = new JButton(new ImageIcon("./img/backbtn.png"));
-	backbtn.setPressedIcon(new ImageIcon("./img/pressedbackbtn.png"));
-	backbtn.setSize(45,45);
-	backbtn.setVisible(true);
-	backbtn.setLocation(15,43);
-	backbtn.setBorderPainted(false);  //버튼 외곽선
-	backbtn.setContentAreaFilled(false);
-	backbtn.setFocusPainted(false);
+	JButton[] sb = new JButton[7]; 
+	public economycontent(){
+		setUndecorated(true);
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice gd = ge.getDefaultScreenDevice();
 	
-	economykeywords economyk = new economykeywords();  //버튼에 키워드를 불러올 객체
-	JButton word1 = new JButton(); JButton word2 = new JButton(); 
-	JButton word3 = new JButton(); JButton word4 = new JButton(); 
-	JButton word5 = new JButton();
-	word1.setText(economyk.getwords(0)); word2.setText(economyk.getwords(1));
-	word3.setText(economyk.getwords(2)); word4.setText(economyk.getwords(3));
-	word5.setText(economyk.getwords(4));
-	word1.setSize(100,45); word2.setSize(100,45); word3.setSize(100,45); word4.setSize(100,45); word5.setSize(100,45);
-	word1.setLocation(65,95); word2.setLocation(65,162); word3.setLocation(65,229); word4.setLocation(65,296); word5.setLocation(65,363);
-	
-	ActionListener listener = new ActionListener(){
-		public void actionPerformed(ActionEvent e){
-			if (e.getActionCommand().equals(ACTION_COMMAND_BACK)){
-				mainframe mf = new mainframe();
-				dispose();
+		a.setLayout(null);
+		JButton backbtn = new JButton(new ImageIcon("./img/backbtn.png"));
+		backbtn.setPressedIcon(new ImageIcon("./img/pressedbackbtn.png"));
+		backbtn.setSize(45,45);
+		backbtn.setVisible(true);
+		backbtn.setLocation(15,43);
+		backbtn.setBorderPainted(false);  //버튼 외곽선
+		backbtn.setContentAreaFilled(false);
+		backbtn.setFocusPainted(false);
+		
+		economykeywords economyk = new economykeywords();  //버튼에 키워드를 불러올 객체
+		JButton word1 = new JButton(); JButton word2 = new JButton(); 
+		JButton word3 = new JButton(); JButton word4 = new JButton(); 
+		JButton word5 = new JButton();
+		word1.setText(economyk.getwords(0)); word2.setText(economyk.getwords(1));
+		word3.setText(economyk.getwords(2)); word4.setText(economyk.getwords(3));
+		word5.setText(economyk.getwords(4));
+		word1.setSize(100,45); word2.setSize(100,45); word3.setSize(100,45); word4.setSize(100,45); word5.setSize(100,45);
+		word1.setLocation(65,95); word2.setLocation(65,162); word3.setLocation(65,229); word4.setLocation(65,296); word5.setLocation(65,363);
+		for (int i = 0; i<7; i++){
+			a.add(sb[i] = new JButton(""));
+			sb[i].setSize(120,45);
+			sb[i].setLocation(625,i*50+95);
+			sb[i].setVisible(false);
 			}
-		}
-	};
-	backbtn.setActionCommand(ACTION_COMMAND_BACK);
-	backbtn.addActionListener(listener);
-	
-	a.add(backbtn);
-	a.add(word1); a.add(word2); a.add(word3); a.add(word4); a.add(word5); //키워드 버튼 추가
-	
-	this.add(a);
-	this.setSize(800,480);
-	this.setVisible(true);
-	//gd.setFullScreenWindow(this); 
+		
+		ActionListener listener = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				if (e.getActionCommand().equals(ACTION_COMMAND_BACK)){
+					mainframe mf = new mainframe();
+					dispose();
+				}
+				if (e.getActionCommand().equals(ACTION_COMMAND_ONE)){
+					int i=0; 
+					while(i<economys.sclength[0]){
+						sb[i].setVisible(true);
+						sb[i].setText(economys.getname(0, i));
+						i++;
+						if (i==7) break;
+					}
+					while(i<7){
+						sb[i].setVisible(false);
+						i++;
+					}
+				}
+				if (e.getActionCommand().equals(ACTION_COMMAND_TWO)){
+					int i=0; 
+					while(i<economys.sclength[1]){
+						sb[i].setVisible(true);
+						sb[i].setText(economys.getname(1, i));
+						i++;
+						if (i==7) break;
+					}
+					while(i<7){
+						sb[i].setVisible(false);
+						i++;
+					}
+				}
+				if (e.getActionCommand().equals(ACTION_COMMAND_THREE)){
+					int i=0; 
+					while(i<economys.sclength[2]){
+						sb[i].setVisible(true);
+						sb[i].setText(economys.getname(2, i));
+						i++;
+						if (i==7) break;
+					}
+					while(i<7){
+						sb[i].setVisible(false);
+						i++;
+					}
+				}
+				if (e.getActionCommand().equals(ACTION_COMMAND_FOUR)){
+					int i=0; 
+					while(i<economys.sclength[3]){
+						sb[i].setVisible(true);
+						sb[i].setText(economys.getname(3, i));
+						i++;
+						if (i==7) break;
+					}
+					while(i<7){
+						sb[i].setVisible(false);
+						i++;
+					}
+				}
+				if (e.getActionCommand().equals(ACTION_COMMAND_FIVE)){
+					int i=0; 
+					while(i<economys.sclength[4]){
+						sb[i].setVisible(true);
+						sb[i].setText(economys.getname(4, i));
+						i++;
+						if (i==7) break;
+					}
+					while(i<7){
+						sb[i].setVisible(false);
+						i++;
+					}
+				}
+			}
+		};
+		backbtn.setActionCommand(ACTION_COMMAND_BACK);
+		backbtn.addActionListener(listener);
+		word1.setActionCommand(ACTION_COMMAND_ONE);
+		word1.addActionListener(listener);
+		word2.setActionCommand(ACTION_COMMAND_TWO);
+		word2.addActionListener(listener);
+		word3.setActionCommand(ACTION_COMMAND_THREE);
+		word3.addActionListener(listener);
+		word4.setActionCommand(ACTION_COMMAND_FOUR);
+		word4.addActionListener(listener);
+		word5.setActionCommand(ACTION_COMMAND_FIVE);
+		word5.addActionListener(listener);
+		
+		a.add(backbtn);
+		a.add(word1); a.add(word2); a.add(word3); a.add(word4); a.add(word5); //키워드 버튼 추가
+		
+		this.add(a);
+		this.setSize(800,480);
+		this.setVisible(true);
+		//gd.setFullScreenWindow(this); 
+	}
 }
-}
-
 class panel extends JPanel
 {
 	ImageIcon bgimg = new ImageIcon("./img/barbackground.png");
